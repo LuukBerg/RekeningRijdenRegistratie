2e5f70c804f421814f712f33fd8dcaa2a075726d
Piet van der Loo <pietvdloo@live.nl>
2019-05-31 17:32:24 +0200

    Fix timestamp format

--- src/main/java/rekeningrijden/fr/rekeningrijdersregistratie/models/StepDTO.java
+++ src/main/java/rekeningrijden/fr/rekeningrijdersregistratie/models/StepDTO.java
@@ -1,31 +1,39 @@
 package rekeningrijden.fr.rekeningrijdersregistratie.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-public class StepDTO {
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 
+@Data
+@NoArgsConstructor
+public class StepDTO
+{
     private double lat;
     private double lon;
+    @JsonFormat(shape = Shape.NUMBER)
     private Date timestamp;
     private String trackerId;
 
-    public static List<StepDTO> transform(List<Step> steps){
-        List<StepDTO> dtos = new ArrayList<>();
-        for (Step step : steps){
-            dtos.add(new StepDTO(step.getX(),step.getY(),step.getTimestamp(),step.getMovement().getCartracker()));
-        }
-        return dtos;
+    private StepDTO(Step step)
+    {
+        lat = step.getX();
+        lon = step.getY();
+        timestamp = step.getTimestamp();
+        trackerId = step.getTrackerId();
     }
 
+    public static List<StepDTO> transform(List<Step> steps)
+    {
+        List<StepDTO> result = new ArrayList<>(steps.size());
+        for (Step step : steps){
+            result.add(new StepDTO(step));
+        }
+        return result;
+    }
 }
