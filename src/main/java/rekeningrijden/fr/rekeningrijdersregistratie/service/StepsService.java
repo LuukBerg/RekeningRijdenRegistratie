e91c81abe7dc1ea7a04706a190e29705d835dc0c
Bkhiti,Rabie R <r.bkhiti@student.fontys.nl>
2019-06-02 18:38:29 +0200

    Continued work on function to retrieve single latest step based on tracker.

--- src/main/java/rekeningrijden/fr/rekeningrijdersregistratie/service/StepsService.java
+++ src/main/java/rekeningrijden/fr/rekeningrijdersregistratie/service/StepsService.java
@@ -31,7 +31,7 @@ public class StepsService
     }
 
     public List<Step> getLatestStepByTracker(String tracker){
-        Pageable pageRequest = PageRequest.of(0,PAGESIZE);
+        Pageable pageRequest = PageRequest.of(0,1);
         return stepRepository.getLatestStepByTracker(tracker, pageRequest);
     }
 }
