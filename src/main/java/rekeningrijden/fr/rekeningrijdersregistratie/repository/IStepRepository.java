24ce8ab9cb0868b7559df6d29e78cb1ac38a4855
Bkhiti,Rabie R <r.bkhiti@student.fontys.nl>
2019-06-02 16:47:58 +0200

    Removed unwanted character in query.

--- src/main/java/rekeningrijden/fr/rekeningrijdersregistratie/repository/IStepRepository.java
+++ src/main/java/rekeningrijden/fr/rekeningrijdersregistratie/repository/IStepRepository.java
@@ -17,6 +17,6 @@ public interface IStepRepository extends PagingAndSortingRepository<Step, Long>
     @Query("SELECT s FROM Step s WHERE s.trackerId = :tracker AND s.timestamp >= :start AND s.timestamp <= :end ORDER BY s.trackerId DESC")
     List<Step> getStepsByTracker(@Param("tracker") String tracker,  @Param("start") Date start, @Param("end") Date end, Pageable pageable);
 
-    @Query("SELECT * FROM registratie.step WHERE tracker_id = :tracker ORDER BY timestamp DESC LIMIT 1;")
+    @Query("SELECT * FROM registratie.step WHERE tracker_id = :tracker ORDER BY timestamp DESC LIMIT 1")
     Step getLatestStepByTracker(@Param("tracker") String tracker);
 }
